f5b891a89b8b3c416421b7f0b831e09dd22e7e9a
Yuri-P00 <yuri.perez1@udea.edu.co>
2024-10-27 22:21:41 -0500

    R5-Creating-Culturemedia-Structures

---
+++ src/main/java/culturemedia/repository/CultureMediaService.java
@@ -0,0 +1,17 @@
+package culturemedia.repository;
+
+import culturemedia.exception.VideoNotFoundException;
+import culturemedia.model.Video;
+import culturemedia.model.View;
+
+import java.util.List;
+
+public interface CultureMediaService {
+    List<Video> findAll() throws VideoNotFoundException;
+    Video save (Video save);
+    View save (View save);
+    List<Video> find(String title) throws VideoNotFoundException;
+    List<Video> find(double fromDuration, double toDuration) throws VideoNotFoundException;
+}
+
+
