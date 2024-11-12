ba36f02d9e574cdebcac479688e41fb01d63887a
YURI PEREZ <yuri.perez1@udea.edu.co>
2024-11-11 22:51:16 -0500

    R12 adding components rest api (#16)
    * Aplicando sugerenccias despues de la revision del codigo
    
    * Mocking–repositories-in-service-test-cases
    
    * Adding-Components-Rest-API

--- src/main/java/culturemedia/controller/CultureMediaController.java
+++ src/main/java/culturemedia/controller/CultureMediaController.java
@@ -2,23 +2,31 @@ package culturemedia.controller;
 
 import java.util.List;
 
+import culturemedia.exception.DuracionNotValidException;
 import culturemedia.exception.VideoNotFoundException;
 import culturemedia.model.Video;
 import culturemedia.service.CultureMediaService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class CultureMediaController {
 
 	private final CultureMediaService cultureMediaService;
-
 
 	public CultureMediaController(CultureMediaService cultureMediaService) {
 		this.cultureMediaService = cultureMediaService;
 	}
 
-
+	@GetMapping("/videos")
 	public List<Video> findAllVideos() throws VideoNotFoundException {
 		return cultureMediaService.findAll();
 	}
 
-
+	@PostMapping("/videos")
+	public Video save(@RequestBody Video video) throws DuracionNotValidException {
+		return cultureMediaService.save(video);
+	}
 }
